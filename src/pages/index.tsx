13033a04997a56998de9ba92e75d1c7e701db264
Filali-anass <anass.filali.93@gmail.com>
2022-08-04 03:40:20 +0100

    fix depl bugs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,20 @@ import { useSession, signIn, signOut } from "next-auth/react";
 import Projects from "../components/Projects";
 import { PROJECTS } from "../lib/data/PROJECTS";
 
-export async function getStaticProps() {
-  const projects = PROJECTS;
+// export async function getStaticProps() {
+//   const projects = PROJECTS;
 
-  return {
-    props: {
-      projects,
-    },
-  };
-}
+//   return {
+//     props: {
+//       projects,
+//     },
+//   };
+// }
 
 const Home = ({ projects }: { projects: typeof PROJECTS }) => {
   const { data: session } = useSession();
 
-  return (
-    <div className={"p-6"}>
-      <Projects projects={projects} />
-    </div>
-  );
+  return <div className={"p-6"}>{/* <Projects projects={projects} /> */}</div>;
 };
 
 export default Home;
